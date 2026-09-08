a6c1574eaea40bb5c544c9ce097d7be6e478581a
Shaheen K. Farjo <shaheen.farjo@gmail.com>
2026-09-08 16:06:00 +0300

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -27,7 +27,7 @@ export function Footer() {
     <footer className="relative">
       {/* Main CTA */}
       <motion.a
-        href="mailto:hello@example.com"
+        href="mailto:hello@shaheenfarjo.com"
         data-cursor-hover
         className="relative block overflow-hidden"
         onMouseEnter={() => setIsHovered(true)}
